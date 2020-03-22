0cfc6b615aa592e0e61eacec88409beac9c6615e
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-03-21 21:32:30 -0700

    Connect modal to habit tracker

--- models/journal.js
+++ models/journal.js
@@ -1,14 +1,46 @@
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
     var Journal = sequelize.define("Journal", {
         // The email cannot be null, and must be a proper email before creation
         name: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true,
+            unique: true
+        },
+        hab1Name: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        hab2Name: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        hab3Name: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        hab4Name: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        hab5Name: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        hab6Name: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        hab7Name: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        hab8Name: {
+            type: DataTypes.STRING,
+            allowNull: true
         }
-    })
+    });
 
-    Journal.associate = function (models) {
+    Journal.associate = function(models) {
         // Journal.belongsTo(models.User, {
         //     foreignKey: {
         //         allowNull: false
@@ -24,6 +56,6 @@ module.exports = function (sequelize, DataTypes) {
         //         allowNull: false
         //     }
         // });
-    }
+    };
     return Journal;
-}
+};
